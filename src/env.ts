a9bd1f12ab58a871e942ac4d36d233f180fe569b
Raziel <raziel@za9.com.br>
2024-09-26 12:27:10 -0300

    Projeto finalizado

---
+++ src/env.ts
@@ -0,0 +1,8 @@
+//este arquivo serve para validar se meu arquivo .env existe
+import z from 'zod'
+
+const envSchema = z.object({
+  DATABASE_URL: z.string().url(),
+})
+
+export const env = envSchema.parse(process.env)
